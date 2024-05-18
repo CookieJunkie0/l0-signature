36ed47138d33d79235b62e59d1e9c6eba45f592a
CookieJunkie0 <rtrt96169@gmail.com>
2024-05-18 20:17:34 +0300

    master

---
+++ main.js
@@ -0,0 +1,18 @@
+const ethers = require('ethers');
+const fs = require('fs');
+
+const MESSAGE = "RFI: I confirm that I own this wallet.";
+
+async function main() {
+    console.log('Made by t.me/cookiejunkieeth')
+    const wallets = fs.readFileSync('./wallets.txt').toString().split('\n');
+
+    for(let wallet of wallets) {
+        wallet = new ethers.Wallet(wallet);
+
+        const msg = await wallet.signMessage(MESSAGE);
+        console.log(wallet.address, msg);
+    }
+}
+
+main()
